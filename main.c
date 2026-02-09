40628dca103a2bed1cf086771bf89ffc800e956b
Egorus2 <106869848+Egorus2@users.noreply.github.com>
2026-02-10 01:53:53 +0300

    init

--- main.c
+++ main.c
@@ -2,21 +2,12 @@
 #include "system_stm32f4xx.h"
 
 #include "system.h" 
-#include "usart.h"
-
-#define TIMEOUT_USART 3UL
-#define MAX_LEN_RX 32UL
 
 
 int main(void)
 {
 	RCC_Init();
 	sysTickInit();
-	GPIO_USART1_Init();
-	USART1_Init();
-	DMA2_USART1_RX_TX_Init();
-
-	usart1_Transm_str("\x1B[2J\x1B[H", TIMEOUT_USART);    // clear the terminal
 	
   while(1)
 	{
