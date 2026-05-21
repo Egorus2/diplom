52eeaff38f7da2ad41c6dcd7371c35195e165f49
Egorus2 <106869848+Egorus2@users.noreply.github.com>
2026-05-21 10:11:20 +0300

    c

--- kalman.c
+++ kalman.c
@@ -2,3 +2,24 @@
 #include "system_stm32f4xx.h"
 
 #include "kalman.h"
+
+void quatMultiply(float *qr, float *q1, float *q2) {
+	qr[0] = q2[0] * q1[0] - q2[1] * q1[1] - q2[2] * q1[2] - q2[3] * q1[3];
+	qr[1] = q2[0] * q1[1] + q2[1] * q1[0] - q2[2] * q1[3] + q2[3] * q1[2];
+	qr[2] = q2[0] * q1[2] + q2[1] * q1[3] + q2[2] * q1[0] - q2[3] * q1[1];
+	qr[3] = q2[0] * q1[3] - q2[1] * q1[2] + q2[2] * q1[1] + q2[3] * q1[0];
+}
+
+
+void normalizeQuat(float *qr, float *q) {
+	float norm;
+
+	norm = 1.0f / sqrtf(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]);
+
+	qr[0] *= norm;
+	qr[1] *= norm;
+	qr[2] *= norm;
+	qr[3] *= norm;
+}
+
+
--- kalman.h
+++ kalman.h
@@ -3,4 +3,15 @@
 
 #include <stdint.h>
 
+    void quatMultiply(float *qr, float *q1, float *q2);
+    void normalizeQuat(float *qr, float *q);
+    void rotateVectorByQuat(float *vr, const float *v, const float *q);
+    void quatToEuler(const float *q, float *roll, float *pitch, float *yaw);
+
+    void crossVector3(float *vr, const float *va, const float *vb);
+    float dotVector3(const float *va, const float *vb);
+    void normalizeVec3(float *vr, const float *v);
+
+    void log_mapQuat_fast(const float *q, float *omega);
+    void exp_mapQuat(const float *omega, float *q);
 #endif 
