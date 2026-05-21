92c048d6235dd81f1552572367f323170c38569f
Egorus2 <106869848+Egorus2@users.noreply.github.com>
2026-05-21 09:48:25 +0300

    c

---
+++ kalman.c
@@ -0,0 +1,4 @@
+#include "stm32f4xx.h"                  // Device header
+#include "system_stm32f4xx.h"
+
+#include "kalman.h"
---
+++ kalman.h
@@ -0,0 +1,6 @@
+#ifndef KALMAN_H
+#define KALMAN_H
+
+#include <stdint.h>
+
+#endif 
